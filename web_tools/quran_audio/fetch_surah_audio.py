57462fdc3a9833eacce1e57c3435ef153e65f978
pythalpy <jaanisark@gmail.com>
2025-08-26 23:56:54 -0700

    set defaults

--- web_tools/quran_audio/fetch_surah_audio.py
+++ web_tools/quran_audio/fetch_surah_audio.py
@@ -4,28 +4,28 @@ from AudioDownloader import AudioDownloader
 list_of_recitors = [
     # Add/Remove/Comment Recitors as Needed
     "abdullah_basfar_w_ibrahim_walk_si", 
-    "noreen_siddiq", 
-    "abdul_basit_murattal", 
-    "abdulbaset_mujawwad",
-    "sudais_and_shuraim_with_urdu", 
-    "abdulbaset_with_naeem_sultan_pickthall", 
-    "khalifah_taniji",
-    "mishaari_raashid_al_3afaasee",
-    "sa3d_al-ghaamidi/complete",
+    # "noreen_siddiq", 
+    # "abdul_basit_murattal", 
+    # "abdulbaset_mujawwad",
+    # "sudais_and_shuraim_with_urdu", 
+    # "abdulbaset_with_naeem_sultan_pickthall", 
+    # "khalifah_taniji",
+    # "mishaari_raashid_al_3afaasee",
+    # "sa3d_al-ghaamidi/complete",
     ]
 
 # Specify surahs to download
 # Can be a single surah, e.g. Sura Yaseen
-# range_of_surahs = (36)
+surahs_to_download = (36)
 
-# Or a range of surahs, e.g. Juz Amma
-range_of_surahs = (1, 114)
+# Or a range of surahs, e.g. Juz 'Amma (from 78 through 114)
+# surahs_to_download = (78, 114)
 
 # Specify Local Download Directory
 local_download_directory = "/path/to/download/folder"
 
 # Prepare and Perform the Download
-audio = AudioDownloader(local_download_directory, list_of_recitors, range_of_surahs)
+audio = AudioDownloader(local_download_directory, list_of_recitors, surahs_to_download)
 result = audio.download_surahs()
 
 # View report of the download
